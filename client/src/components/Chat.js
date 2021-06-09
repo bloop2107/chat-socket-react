5d77eb0d10dfe6064e8682f04eed077829b525b8
Thai Bao <jackymad06@gmail.com>
2021-06-09 22:05:38 +0700

    Connect socket IO

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({location}) => {
+    const [name,setName] = useState('');
+    const [room,setRoom] = useState('');
+    const [message,setMessage] = useState('');
+    const [messages,setMessages] = useState([]);
+
+    const ENDPOINT = 'http://127.0.0.1:5000/';
+
+    
+
+    useEffect(() => {
+        const {name,room} = queryString.parse(location.search); 
+
+        socket = io('http://127.0.0.1:5000/',{
+            transports: ['websocket']
+        });
+
+        socket.emit('join', { name,room }, () => {
+        });
+
+        
+
+        setName(name);
+        setRoom(room); 
+
+        return () => {
+            socket.emit('disconnected');
+
+            socket.off();
+        }
+
+    },[ENDPOINT,location.search])
+
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages,message]); 
+        });
+    }, [messages]);
+
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage',message, (res) => {
+                setMessage('');
+            })
+        }
+    }
+
+    // console.log(message,messages);
+
     return (
         <div>
-            <h1>Chat</h1>
+            <input 
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null }
+            />
         </div>
     )
 }
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -2,10 +2,70 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name,setName] = useState('');
+    const [room,setRoom] = useState('');
+
     return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <div>
-            <h1 className="text-red-500">Join</h1>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Join</h2>
         </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="name" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="name"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Enter your name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="room" className="sr-only">
+                Password
+              </label>
+              <input
+                id="room"
+                name="room"
+                type="text"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mt-2"
+                placeholder="Select Room"
+                onChange={(e) => setRoom(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+              <Link onClick={(e) => (!name || !room) ? e.preventDefault() : null } to={`/chat?name=${name}&room=${room}`} >
+                <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                </span>
+                Sign in
+                </button>
+              </Link>
+          </div>
+        </form>
+      </div>
+    </div>
     )
 }
 
